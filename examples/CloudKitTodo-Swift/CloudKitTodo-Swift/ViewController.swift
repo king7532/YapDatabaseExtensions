3e81ab0c3be746330630a87b120e924695834f4b
Benjamin King <king7532@mac.com>
2015-06-23 17:35:40 -0400

    Create initial project for CloudKitTodo-Swift in the `examples` folder using Xcode 6.3.2 (Swift 1.2)

---
+++ examples/CloudKitTodo-Swift/CloudKitTodo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudKitTodo-Swift
+//
+//  Created by king on 6/23/15.
+//  Copyright (c) 2015 Example.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
